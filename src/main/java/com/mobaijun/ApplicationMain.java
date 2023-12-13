9bdf81a92ccc8d9ff75f20150619310bb5a56b74
mobaijun <mobaijun8@163.com>
2023-12-13 08:47:15 +0800

    :sparkles: 新增按照仓库语言分类并自动部署到 vercel

--- src/main/java/com/mobaijun/ApplicationMain.java
+++ src/main/java/com/mobaijun/ApplicationMain.java
@@ -1,7 +1,6 @@
 package com.mobaijun;
 
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.lang.Console;
 import cn.hutool.core.text.StrBuilder;
 import com.mobaijun.github.Github;
 import com.mobaijun.github.Repository;
@@ -12,6 +11,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * 主程序
@@ -20,6 +23,11 @@ import java.util.List;
  * @author mobai
  */
 public class ApplicationMain {
+
+    /**
+     * 获取 Logger 实例
+     */
+    private static final Logger logger = Logger.getLogger(ApplicationMain.class.getName());
 
     /**
      * 文件地址
@@ -44,8 +52,8 @@ public class ApplicationMain {
         // 标题
         sb.append("# ").append(username).append(" Starred Repositories\n\n");
         List<Repository> repositoryList = Github.getGithubStarList(username);
-        // 打印日志
-        Console.log(repositoryList);
+        // 写入 html
+        writeRepositoriesToHtml(repositoryList);
         // 拼接列表
         repositoryList.forEach(item -> {
             String name = item.getName();
@@ -58,5 +66,46 @@ public class ApplicationMain {
         });
         // 打印
         FileUtil.writeString(sb.toString(), PATH.toAbsolutePath().toString(), StandardCharsets.UTF_8);
+    }
+
+
+    /**
+     * 写入 html 文件
+     *
+     * @param repositoryList 仓库列表
+     */
+    public static void writeRepositoriesToHtml(List<Repository> repositoryList) {
+        // 使用 Stream API 对 repositoryList 进行分组，将语言为空的仓库单独放在一个列表中
+        Map<String, List<Repository>> groupedByLanguage = repositoryList.stream()
+                .collect(Collectors.groupingBy(repository -> repository.getLanguage() != null ? repository.getLanguage() : "Unknown"));
+
+        // 构建 HTML 字符串
+        StringBuilder htmlContent = new StringBuilder("<!doctype html><html lang=\"en\">" +
+                "<head>" +
+                "<meta charset=\"UTF-8\">" +
+                "<meta content='width=device-width initial-scale=1' name='viewport'><title>Repo</title></head><body>" +
+                "<h1 id='mobaijun-starred-repositories'>mobaijun Starred Repositories</h1>");
+
+        groupedByLanguage.forEach((language, repositories) -> {
+            htmlContent.append("<h2>").append(language).append("</h2><ul>");
+            repositories.forEach(repository -> {
+                htmlContent.append("<li>")
+                        .append("<a href=\"").append(repository.getHtmlUrl()).append("\" target=\"_blank\">")
+                        .append(repository.getName()).append("</a>")
+                        .append(" [").append(repository.getDescription()).append("]")
+                        .append("</li>");
+            });
+            htmlContent.append("</ul>");
+        });
+
+        htmlContent.append("</body></html>");
+
+        // 写入 HTML 文件
+        try {
+            Files.writeString(Path.of("docs/repositories.html"), htmlContent);
+            System.out.println("HTML file created successfully.");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error writing HTML file", e);
+        }
     }
 }
